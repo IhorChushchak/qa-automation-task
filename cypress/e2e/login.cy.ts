94663604492573a9bee940c164d9cd35a6e8832b
Ihor Chushchak <gor-chushhak@ukr.net>
2024-06-22 11:19:13 +0300

    Added Assertions

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,8 +1,10 @@
 /// <reference types="cypress" />
 
 import LoginPage from '../support/pageObjects/LoginPage';
+import * as chai from 'chai';
 
 const loginPage = new LoginPage();
+const assert = chai.assert;
 
 describe('Login Page Tests', () => {
   beforeEach(() => {
@@ -10,22 +12,32 @@ describe('Login Page Tests', () => {
   });
 
   it('should display all elements on the page', () => {
-    loginPage.getUsernameField().should('be.visible');
-    loginPage.getPasswordField().should('be.visible');
-    loginPage.getLoginButton().should('be.visible');
+    loginPage.getUsernameField().should('be.visible').then($el => {
+      assert.isNotNull($el, 'Username field is visible');
+    });
+    loginPage.getPasswordField().should('be.visible').then($el => {
+      assert.isNotNull($el, 'Password field is visible');
+    });
+    loginPage.getLoginButton().should('be.visible').then($el => {
+      assert.isNotNull($el, 'Login button is visible');
+    });
   });
 
   it('should display error messages for invalid login', () => {
     cy.fixture('invalidCredentials').then((invalidCredentials) => {
       invalidCredentials.forEach((credentials) => {
         loginPage.login(credentials.username, credentials.password);
-        loginPage.getErrorMessage().should('be.visible');
+        loginPage.getErrorMessage().should('be.visible').then($el => {
+          assert.include($el.text(), 'No account found with that username.', 'Error message is visible and correct');
+        });
       });
     });
   });
 
   it('should fail due to unsuccessful login', () => {
     loginPage.login('invalidUser', 'invalidPassword');
-    loginPage.getErrorMessage().should('be.visible');
+    loginPage.getErrorMessage().should('be.visible').then($el => {
+      assert.include($el.text(), 'No account found with that username.', 'Error message is visible and correct');
+    });
   });
 });
